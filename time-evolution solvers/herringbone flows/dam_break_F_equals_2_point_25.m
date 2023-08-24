f5cfcd46277b32629f77a57a9a75e3c12257d6ff
Zhao Yang <112669623+zyang-pde@users.noreply.github.com>
2023-08-24 15:34:17 +0800

    Update dam_break_F_equals_2_point_25.m

--- time-evolution solvers/herringbone flows/dam_break_F_equals_2_point_25.m
+++ time-evolution solvers/herringbone flows/dam_break_F_equals_2_point_25.m
@@ -1,7 +1,7 @@
 clc
 clear
 filepath=strcat(pwd,'\dam_break_F_equals_2_point_25');
-N=4000;
+N=5000;
 mov(1:N)= struct('cdata',[],'colormap',[]);
 filenamev='dambreak_F_equals_2_point_25';
 X_lower=0.005;
@@ -9,23 +9,25 @@ X_upper=19.995;
 dx=0.01;
 Y_lower=0.005;
 Y_upper=4.995;
-
 dy=0.01;
 [X,Y] = meshgrid(X_lower:dx:X_upper,Y_lower:dy:Y_upper);
 [m,n]=size(X);
-
-dt=0.1;
+dt=0.2;
 for i=1:N
+    if i==1
+        f=figure;
+        f.Color = 'black';
+    end
     if i-1<=9
         format='fort.q000%d';
     else if i-1<=99
             format='fort.q00%d';
-        else if i-1<=999
-                format='fort.q0%d';
-            else
-                format='fort.q%d';
-            end
-        end
+    else if i-1<=999
+            format='fort.q0%d';
+    else
+        format='fort.q%d';
+    end
+    end
     end
     filename=fullfile(filepath,sprintf(format,i-1));
     fileID=fopen(filename);
@@ -34,17 +36,15 @@ for i=1:N
     H=C{1};
     H=reshape(H,[n,m]);
     H=transpose(H);
-    if i==1
-       f=figure;
-    end
+
     ax=axes;
     s=surf(X,Y,H);
     s.EdgeColor='none';
     view(3)
     ax.XLim = [0 20];
     ax.ZLim = [0 5];
-    ax.CameraPosition =[10.5 -6 5];
-    ax.CameraTarget = [10.5 2.5 1];
+    ax.CameraPosition =[12 -6 5];
+    ax.CameraTarget = [10 2.5 1];
     ax.CameraUpVector = [0 0 1];
     ax.CameraViewAngle = 25;
     ax.DataAspectRatio = [1 1 1];
@@ -62,7 +62,7 @@ for i=1:N
     s.SpecularStrength = 1;
     s.SpecularColorReflectance = 1;
     s.SpecularExponent = 7;
-     t = annotation('textbox','String','$F=2.25$, $H_R=0.7$, channel width is $5$ and length is $20$','Interpreter','latex','EdgeColor','none');
+    t = annotation('textbox','String','$F=2.25$, $H_R=0.7$, channel width is $5$ and length is $20$','Interpreter','latex','EdgeColor','none');
     t.FontSize=20;
     t.Color='white';
     t.Position=[0.27,0.05,0.4,0.12];
@@ -72,9 +72,7 @@ for i=1:N
     t.FontSize=20;
     t.Color='white';
     t.Position=[0.5,0.8,0.4,0.12];
-    
     axis off
-    f.Color = 'black';
     mov(i)=getframe(f);
     clf(f)
 end
